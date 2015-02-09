0e750d13462a250c8655650af0fe5d6032e5ca7a
kobayashi <shoji.kobayashi.0125@gmail.com>
2015-02-09 22:35:26 +0900

    add login and logout on UsersController

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -15,6 +15,26 @@ class UsersController extends AppController {
  */
 	public $components = array('Paginator');
 
+public function beforeFilter() {
+    parent::beforeFilter();
+    // ユーザー自身による登録とログアウトを許可する
+    $this->Auth->allow('add', 'logout');
+}
+
+public function login() {
+    if ($this->request->is('post')) {
+        if ($this->Auth->login()) {
+            $this->redirect($this->Auth->redirect());
+        } else {
+            $this->Session->setFlash(__('Invalid username or password, try again'));
+        }
+    }
+}
+
+public function logout() {
+    $this->redirect($this->Auth->logout());
+}
+
 /**
  * index method
  *
